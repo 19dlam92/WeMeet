46a090d34b862b39fe89886e27a25d67c62b4bf4
19dlam92 <19dlam92@gmail.com>
2022-04-21 09:56:51 -0400

    backend is connected. now building the frontend

---
+++ client/src/components/ProfileDetails.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const ProfileDetails = () => {
+
+    
+
+
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default ProfileDetails;
--- server/routes/datingProfile.route.js
+++ server/routes/datingProfile.route.js
@@ -1,7 +1,7 @@
 const DatingProfileController = require('../controllers/datingProfile.controller');
 
 module.exports = (app) => {
-    app.get("/api/DatingProfile", DatingProfileController.findAllDatingProfile);
+    app.get("/api/DatingProfile", DatingProfileController.findAllDatingProfiles);
     app.get("/api/DatingProfile/:id", DatingProfileController.findOneDatingProfile);
     app.put("/api/DatingProfile/:id", DatingProfileController.updateOneDatingProfile);
     app.post("/api/DatingProfile", DatingProfileController.createNewDatingProfile);
