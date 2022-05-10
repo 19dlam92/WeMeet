4ef396d366a28f6fef92ffc469db547991d1b93f
19dlam92 <19dlam92@gmail.com>
2022-05-10 13:01:14 -0400

    image upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use( cors({ credentials: true, origin: 'http://localhost3000/' }) )
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) );
 app.use( cookieParser() );
+app.use( express.static("images") );
 require('dotenv').config();
 require("./server/config/mongoose.config")
 require("./server/routes/datingProfile.route")(app)
--- server/routes/datingProfile.route.js
+++ server/routes/datingProfile.route.js
@@ -1,4 +1,31 @@
 const DatingProfileController = require('../controllers/datingProfile.controller');
+const multer = require('multer');
+const { v4: uuid4 } = require('uuid');
+const { path } = require('express/lib/application');
+
+const storage = multer.diskStorage({
+    destination: function( request, file, cb ) {
+        cb(null, 'images');
+    },
+    filename: function( request, file, cb ) {
+        cb(null, uuid4() + '-' + Date.now() + path.extname(file.originalname))
+        // uuid = generates random ID per upload
+        // '-' = literal hyphen
+        // Date.now = timestamp
+        // path.extname = where file was uploaded
+    }
+})
+
+const fileFilter = ( request, file, cb ) => {
+    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if (allowedFileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false)
+    }
+}
+
+const imageUpload = multer({ storage, fileFilter });
 
 module.exports = (app) => {
     app.get("/api/datingProfile", DatingProfileController.findAllDatingProfiles);
